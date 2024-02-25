48dced2f299a803d587ad6dff27ba220a2ee54ed
tapadar13 <monsurismyname@gmail.com>
2024-02-25 10:45:13 +0530

    feat: Add heading section to landing page
    - Introduce a prominent heading with a message
    - Incorporate a subheading describing the purpose of Jotion
    - Include a button to enter Jotion

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./_components/Heading";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-f-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="text-5xl font-bold text-black-900">Hello, World!</div>;
-}
