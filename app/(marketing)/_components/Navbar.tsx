994b70e17dddf80b0d174f7aed4501a7b26dc77b
tapadar13 <monsurismyname@gmail.com>
2024-02-25 13:57:02 +0530

    feat: Integrate Navigation Bar into landing page
    - Create a responsive navigation bar for improved user navigation
    - Enhance overall user experience with accessible navigation

---
+++ app/(marketing)/_components/Navbar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Logo } from "./Logo";
+
+export const Navbar = () => {
+  const scrolled = useScrollTop();
+
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        <Button variant="ghost" size="sm">
+          Log in
+        </Button>
+      </div>
+    </div>
+  );
+};
