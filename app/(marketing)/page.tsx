263266c086a80a9ffbe29e9cc1fd629bf8426463
tapadar13 <monsurismyname@gmail.com>
2024-02-25 13:25:21 +0530

    feat: Implement Hero and Footer sections
    - Introduce a Hero section with captivating content
    - Include a Footer section for additional information
    - Enhance overall layout and aesthetics

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Heading from "./_components/Heading";
+import { Heroes } from "./_components/Heroes";
+import { Footer } from "./_components/Footer";
 
 const MarketingPage = () => {
   return (
     <div className="min-f-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
+        <Heroes />
       </div>
+      <Footer />
     </div>
   );
 };
