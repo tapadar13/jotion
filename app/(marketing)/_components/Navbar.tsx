1d4fd2f00bdee62e67478a987cbe359899ccc9e7
tapadar13 <monsurismyname@gmail.com>
2024-02-26 15:31:48 +0530

    feat: Implement Spinner component for asynchronous loading in Navbar
    - Introduce Spinner component with configurable size variants
    - Enhance Navbar with Spinner to indicate loading state during asynchronous operations
    - Improve user experience by providing visual feedback for loading actions

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -3,11 +3,15 @@
 import Link from "next/link";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -19,6 +23,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <Button variant="ghost" size="sm">
           <ModeToggle />
         </Button>
