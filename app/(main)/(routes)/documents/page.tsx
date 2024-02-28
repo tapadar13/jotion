47d64f72c35eeb61beb7878e7cac980e25d5c1a4
tapadar13 <monsurismyname@gmail.com>
2024-02-28 11:04:56 +0530

    feat: Implement private Documents route
    - Create a private route for Documents requiring authentication
    - Introduce MainLayout component for consistent layout structure
    - Implement redirection for unauthenticated users to the homepage

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>This is a protected Documents page.</div>;
+};
+
+export default page;
---
+++ app/(main)/_components/Navigation.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navigation = () => {
+  return <div>Navigation</div>;
+};
+
+export default Navigation;
+
+// (
+//     <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+//       <div>
+//         <p>Action Items</p>
+//       </div>
+//       <div className="mt-4">
+//         <p>Documents</p>
+//       </div>
+//       <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"></div>
+//     </aside>
+//   );
