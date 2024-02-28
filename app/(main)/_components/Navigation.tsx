e2958161eb0141b18896f34fd53fb980322a36d7
tapadar13 <monsurismyname@gmail.com>
2024-02-28 11:51:09 +0530

    feat: Implement responsive sidebar behavior
    - Introduce collapsing sidebar on mobile screens for improved user experience
    - Maintain a fixed sidebar on larger screens for consistent navigation
    - Enhance usability with a smooth transition effect during resizing

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+import { cn } from "@/lib/utils";
+import { ChevronsLeft } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <aside
+      ref={sidebarRef}
+      className={cn(
+        "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+        isResetting && "transition-all ease-in-out duration-300",
+        isMobile && "w-0"
+      )}
+    >
+      <div
+        role="button"
+        className={cn(
+          "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+          isMobile && "opacity-100"
+        )}
+      >
+        <ChevronsLeft className="h-6 w-6" />
+      </div>
+      <div>
+        <p>Action Items</p>
+      </div>
+      <div className="mt-4">
+        <p>Documents</p>
+      </div>
+      <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"></div>
+    </aside>
+  );
 };
 
 export default Navigation;
-
-// (
-//     <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
-//       <div>
-//         <p>Action Items</p>
-//       </div>
-//       <div className="mt-4">
-//         <p>Documents</p>
-//       </div>
-//       <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"></div>
-//     </aside>
-//   );
