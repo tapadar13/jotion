88e71dc91c6e49af13d4eb344561fd9b1a01d672
tapadar13 <monsurismyname@gmail.com>
2024-03-03 20:28:44 +0530

    feat: Enable Recursive Rendering in DocumentList Component
    - Implement recursive rendering in DocumentList for nested document structures
    - Introduce dynamic padding to visually represent hierarchy levels
    - Enhance the user interface by allowing collapsible document lists
    - Display a message when no pages are present inside a document

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import Item from "./Item";
+import { FileIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface DocumentListProps {
   parentDocumentId?: Id<"documents">;
@@ -47,7 +49,40 @@ const DocumentList = ({ parentDocumentId, level = 0 }: DocumentListProps) => {
     );
   }
 
-  return <div>DocumentList</div>;
+  return (
+    <>
+      <p
+        style={{
+          paddingLeft: level ? `${level * 12 + 25}px` : undefined,
+        }}
+        className={cn(
+          "hidden text-sm font-medium text-muted-foreground/80",
+          expanded && "last:block",
+          level === 0 && "hidden"
+        )}
+      >
+        No pages inside
+      </p>
+      {documents.map((document) => (
+        <div key={document._id}>
+          <Item
+            id={document._id}
+            onClick={() => onRedirect(document._id)}
+            label={document.title}
+            icon={FileIcon}
+            documentIcon={document.icon}
+            active={params.documentId === document._id}
+            level={level}
+            onExpand={() => onExpand(document._id)}
+            expanded={expanded[document._id]}
+          />
+          {expanded[document._id] && (
+            <DocumentList parentDocumentId={document._id} level={level + 1} />
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default DocumentList;
