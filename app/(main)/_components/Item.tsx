290662d91ccde2e459665e5cdebc67b047483a9b
tapadar13 <monsurismyname@gmail.com>
2024-03-03 10:43:37 +0530

    style: Improve hierarchy item rendering component
    - Enhance visual presentation of hierarchy items with consistent styling
    - Adjust padding based on hierarchy level for improved readability
    - Introduce conditional rendering for document icons or default icons
    - Improve accessibility with proper role and hover effects

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,22 +1,59 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 
 interface ItemProps {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
   label: string;
   onClick: () => void;
   icon: LucideIcon;
 }
 
-const Item = ({ label, onClick, icon: Icon }: ItemProps) => {
+const Item = ({
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary"
+      )}
     >
-      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
+          onClick={() => {}}
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
+        </div>
+      )}
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
     </div>
   );
