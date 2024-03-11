2f17975d6cfd3de4f86af0e9eb7d11f7f17a5f58
tapadar13 <monsurismyname@gmail.com>
2024-03-11 12:54:46 +0530

    feat: Implement Document Menu for Enhanced User Actions
    This commit introduces a "Menu" component enabling users to perform actions like archiving documents. This feature provides a clean and accessible menu for enhanced document management. Additionally, a user-friendly Skeleton loader is added for improved UI experience.

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -9,6 +9,7 @@ import { MenuIcon } from "lucide-react";
 
 import { Title } from "./Title";
 import { Banner } from "./Banner";
+import { Menu } from "./Menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -46,6 +47,9 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
