e3aa598181f943960e841754991818e356a0b071
tapadar13 <monsurismyname@gmail.com>
2024-03-12 10:32:35 +0530

    feat: Implement Cover Image Modal for Enhanced User Experience
    - Provided users with a dedicated modal for cover image management. The modal includes a clean and centered design, enhancing the overall user experience when adding or replacing cover images.

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -5,6 +5,7 @@ import { ImageIcon, Smile, X } from "lucide-react";
 import { useMutation } from "convex/react";
 import TextareaAutosize from "react-textarea-autosize";
 
+import { useCoverImage } from "@/hooks/use-cover-image";
 import { Doc } from "@/convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
@@ -22,6 +23,9 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
   const [value, setValue] = useState(initialData.title);
 
   const update = useMutation(api.documents.update);
+  const removeIcon = useMutation(api.documents.removeIcon);
+
+  const coverImage = useCoverImage();
 
   const enableInput = () => {
     if (preview) return;
@@ -100,7 +104,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
         )}
         {!initialData.coverImage && !preview && (
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
