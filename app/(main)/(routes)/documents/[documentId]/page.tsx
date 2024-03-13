7b89aa4d73a9f1f3d5495f3510a4d5e7ec0a8669
tapadar13 <monsurismyname@gmail.com>
2024-03-13 11:55:12 +0530

    feat: Integrate Blocknote text editor component
    This commit introduces the Blocknote text editor component, facilitating rich text content creation and editing. The "Editor" component leverages the Blocknote library to provide comprehensive functionalities, including content editing, theme support, and content change tracking. This integration enhances the application's capabilities, empowering users to seamlessly create and modify textual content.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -8,6 +8,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
+import Editor from "@/components/editor";
 
 interface DocumentIdPageProps {
   params: {
@@ -32,6 +33,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   if (document === undefined) {
     return (
       <div>
+        <Cover.Skeleton />
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
             <Skeleton className="h-14 w-[50%]" />
@@ -53,6 +55,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={() => {}} initialContent={document.content} />
       </div>
     </div>
   );
